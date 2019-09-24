c2678c464a4ac77909bf410a91dab652c4dd527e
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-09-24 15:02:59 +1000

    Added JSDocs to classes (#4)

--- src/models/Idea.ts
+++ src/models/Idea.ts
@@ -9,6 +9,13 @@ export default class Idea extends BaseReference<IdeaData, UpdateIdea> {
 	static all = db.collection("ideas");
 	static subscribe = Idea.all.onSnapshot;
 
+	/**
+	 * Creates an Idea instance from a Firestore Document Snapshot.
+	 * `data` property is populated from snapshot and can be
+	 * accessed ***WITHOUT*** calling `init()`
+	 * @param snap Document Snapshot from a Firestore Idea
+	 * @returns Pre-initialised Instance of existing Idea
+	 */
 	static fromExisting(snap: fs.DocumentSnapshot) {
 		if (!snap.exists) throw new Error(`Idea does not exist: ${snap.id}`);
 
@@ -18,10 +25,24 @@ export default class Idea extends BaseReference<IdeaData, UpdateIdea> {
 		return existing;
 	}
 
+	/**
+	 * Similar to `.fromExisting` but used with a query of documents.
+	 * `data` property of each instance is populated from snapshot
+	 * and can be accessed ***WITHOUT*** calling `init()`
+	 * @param querySnap Query Snapshot from a Firestore Idea query
+	 * @returns Array of pre-initialised Idea instances.
+	 */
 	static fromCollection(querySnap: fs.QuerySnapshot) {
 		return querySnap.docs.map(doc => Idea.fromExisting(doc));
 	}
 
+	/**
+	 * Creates and writes a new idea to Firestore.
+	 * `data` property is populated from snapshot and can be
+	 * accessed ***WITHOUT*** calling `init()`
+	 * @param initData Values used when creating the new Idea
+	 * @returns Pre-initialised instance of the created Idea
+	 */
 	static async create(initData: NewIdea) {
 		const payload: IdeaData = {
 			...initData,
@@ -46,6 +67,12 @@ export default class Idea extends BaseReference<IdeaData, UpdateIdea> {
 	}
 	// End Static
 
+	/**
+	 * If no intial value is passed, a *new* document reference will be created within the ideas collection.
+	 *
+	 * Instance is **NOT** written to Firestore until calling `.ref.set` *OR* setting the `data` property.
+	 * @param init Optional param for existing documents
+	 */
 	constructor(init?: string | fs.DocumentReference) {
 		super("ideas", init);
 	}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,31 @@
 import BaseReference from "./BaseReference";
 import { UserData, UpdateUser, Skill } from "./typings";
 
+/**
+ * If no intial value is passed, a *new* document reference will be created within the passed collection.
+ *
+ * Instance is **NOT** written to Firestore until calling `.ref.set` *OR* setting the `data` property.
+ * @classdesc
+ * @param collection The target Firestore collection (e.g. "ideas")
+ * @param init Optional param for existing documents
+ */
 export default class User extends BaseReference<UserData, UpdateUser> {
+	/**
+	 * If no intial value is passed, a *new* document reference will be created within the users collection.
+	 *
+	 * Instance is **NOT** written to Firestore until calling `.ref.set` *OR* setting the `data` property.
+	 * @param init Optional param for existing documents
+	 */
 	constructor(init) {
 		super("users", init);
 	}
 
+	/**
+	 * Adds a new skill to the user and writes to Firestore.
+	 * Throws an Error if skill is already know.
+	 * @param newSkill Skill to be added - **must** be from `Skill` Enum
+	 * @requires src/models/typings
+	 */
 	async addSkill(newSkill: Skill) {
 		this.checkInit();
 
@@ -15,6 +35,12 @@ export default class User extends BaseReference<UserData, UpdateUser> {
 		});
 	}
 
+	/**
+	 * Removes skilsl from the user and writes to Firestore.
+	 * Throws an Error if skill is not known.
+	 * @param toRemove Skill to be removed - **must** be from `Skill` Enum
+	 * @requires src/models/typings
+	 */
 	async removeSkill(toRemove: Skill) {
 		this.checkInit();
 
