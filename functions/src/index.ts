1090aa088df9c54c4b732f85fcbb83f8baf87e5e
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-09-26 13:40:48 +1000

    firebase function for aggregating recent likes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp(functions.config().firebase);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +8,36 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const aggregateIdeaOwner = functions.firestore
+	.document("users/{userId}")
+	.onWrite(async ({ before, after }) => {
+		const sameName = before.exists && before.data()!.name == after.data()!.name;
+
+		if (!sameName) {
+			const qs = await admin
+				.firestore()
+				.collection("ideas")
+				.where("owner.ref", "==", after.ref)
+				.get();
+
+			const ownerIdeaRefs = qs.docs.map(doc => doc.ref);
+
+			const batch = admin.firestore().batch();
+
+			ownerIdeaRefs.forEach(ideaRef =>
+				batch.update(ideaRef, {
+					owner: {
+						name: after.data()!.name,
+						ref: after.ref
+					}
+				})
+			);
+
+			batch.commit();
+		}
+	});
+
+export const aggregateRecentIdeaLikes = functions.firestore
+	.document("ideas/{ideaId}")
+	.onWrite(async ({ before, after }) => {});
