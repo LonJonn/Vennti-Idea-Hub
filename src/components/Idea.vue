f6710c1b1896de0653f4f7df4c676edf942385cf
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-10-04 11:35:41 +1000

    init

--- src/components/Idea.vue
+++ src/components/Idea.vue
@@ -1,48 +1,35 @@
 <template>
 	<div class="mb-5">
-		<div v-if="!loading && idea.data" class="list bg-gray-300 rounded text-blue-800 p-5">
+		<div class="list bg-gray-300 rounded text-blue-800 p-5">
 			<li>description: {{ idea.data.description }}</li>
 			<li>benefit: {{ idea.data.benefit }}</li>
 			<li>skills required: {{ readableSkills }}</li>
 			<li>opened: {{ idea.data.createdOn.toDate() }}</li>
-			<li>likes: {{ idea.data.likesCount }}</li>
 			<li>status: {{ readableStatus }}</li>
 			<br />
-			<li>Owner: {{ this.idea.data.owner.name }}</li>
-			<button v-if="!userHasLiked" class="like" @click="likes.add()">Like</button>
-			<button v-else class="like" @click="likes.remove()">Unlike</button>
-			<button class="close" @click="deleteIdea()">Delete</button>
-			{{ recentLikes }}
+			<li>Owner: {{ idea.data.owner.name }}</li>
+			<AppLikes :id="idea.id" />
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import AppLikes from "@/components/Likes.vue";
+
 import { User, Idea } from "@/models";
 import { Skill, IdeaStatus, Like } from "@/models/typings";
 import { State } from "vuex-class";
 import { db } from "../firebase";
 import { Likes } from "../models";
 
-@Component
+@Component({ components: { AppLikes } })
 export default class IdeaComponent extends Vue {
 	// Mapped Store
 	@State user: User;
 
 	// Props
 	@Prop() idea: Idea;
-
-	// Data
-	loading: boolean = true;
-	likes: Likes = null;
-
-	// Hooks
-	async mounted() {
-		this.likes = await new Likes(this.idea.id).init();
-
-		this.loading = false;
-	}
 
 	// Methods
 	deleteIdea() {
@@ -59,32 +46,16 @@ export default class IdeaComponent extends Vue {
 	get readableStatus() {
 		return IdeaStatus[this.idea.data.status];
 	}
-
-	get recentLikes() {
-		return this.likes.byLatest
-			.splice(0, 2)
-			.map(like => (like.ref.id === this.user.id ? "You" : like.name))
-			.join(", ");
-	}
-
-	get userHasLiked() {
-		return this.likes.userIds.some(id => id === this.user.id);
-	}
 }
 </script>
 
-<style lang="postcss" scoped>
+<style lang="postcss">
 .list > li {
 	@apply capitalize;
 }
 
 .button {
 	@apply text-lg text-white font-semibold px-6 py-1 rounded mt-2;
-}
-
-.like {
-	@apply button;
-	@apply bg-green-500;
 }
 
 .close {
---
+++ src/components/Likes.vue
@@ -0,0 +1,56 @@
+<template>
+	<div v-if="!loading">
+		<p>Likes: {{ likes.count }}</p>
+		<button v-if="!userHasLiked" class="like" @click="likes.add()">Like</button>
+		<button v-else class="like" @click="likes.remove()">Unlike</button>
+		<button class="close" @click="deleteIdea()">Delete</button>
+		{{ recentLikes }}
+	</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { User, Likes } from "@/models";
+import { State } from "vuex-class";
+
+@Component
+export default class LikesComponent extends Vue {
+	// Mapped Store
+	@State user: User;
+
+	// Props
+	@Prop() id: string;
+
+	likes: Likes;
+	loading = true;
+
+	// Lifecycle
+	async mounted() {
+		this.likes = await new Likes(this.id).init();
+		this.loading = false;
+	}
+
+	// Computed
+	get recentLikes() {
+		return this.likes.byLatest
+			.splice(0, 2)
+			.map(like => (like.ref.id === this.user.id ? "You" : like.name))
+			.join(", ");
+	}
+
+	get userHasLiked() {
+		return this.likes.userIds.some(id => id === this.user.id);
+	}
+}
+</script>
+
+<style lang="postcss" scoped>
+.button {
+	@apply text-lg text-white font-semibold px-6 py-1 rounded mt-2;
+}
+
+.like {
+	@apply button;
+	@apply bg-green-500;
+}
+</style>
