9e4c5372ec27f3872d2d77c80bf0fa630b2efd99
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-11-23 20:59:49 +1100

    Global components

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,14 @@ auth.setPersistence("local");
 import preloader from "@/assets/preloader";
 document.getElementById("app").appendChild(preloader);
 
+const requireComponent = require.context("./components/Base", false, /\.vue$/);
+for (const filename of requireComponent.keys()) {
+	const componentConfig = requireComponent(filename);
+	const componentName = `App${filename.substr(2, filename.length - 6)}`;
+	console.log(componentName);
+	Vue.component(componentName, componentConfig.default || componentConfig);
+}
+
 const unsubscribe = auth.onAuthStateChanged(() => {
 	new Vue({
 		router,
