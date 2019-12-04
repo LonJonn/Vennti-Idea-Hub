9919dc718b5752634adf0c1147d84e71c2ab72e0
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-12-04 14:48:31 +1100

    Cleaned up create page

---
+++ src/components/Create/Helper.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="helper">
+		<h2 class="mb-4">Make sure your idea is</h2>
+		<ul class="list">
+			<li>
+				<i class="check-icon far fa-check-circle"></i>
+				<p class="title">Descriptive</p>
+				<p
+					class="sub"
+				>Make sure you thoroughly outline why your idea is great and why others would want to contribute!</p>
+			</li>
+			<li>
+				<i class="check-icon far fa-check-circle"></i>
+				<p class="title">Unique</p>
+				<p class="sub">Check to see if someone has already put a similar idea up</p>
+			</li>
+			<li>
+				<i class="check-icon far fa-check-circle"></i>
+				<p class="title">Impactful</p>
+				<p class="sub">Great ideas will help grow the company through your feedback and recommendations</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelperComponent extends Vue {}
+</script>
+
+<style lang="postcss" scoped>
+.helper {
+	@apply bg-gray-100 rounded-xl shadow-xl p-12 w-2/5 hidden overflow-hidden relative;
+}
+
+.helper > h2 {
+	@apply font-bold text-2xl;
+}
+
+.helper::before {
+	content: "";
+	@apply absolute block h-24 w-24 rounded-full bg-gray-400 opacity-75;
+	bottom: -1.75em;
+	left: 3em;
+}
+
+.helper::after {
+	content: "";
+	@apply absolute block h-8 w-4/5 bg-gray-400 opacity-75;
+	bottom: 2em;
+	left: -5em;
+	transform: rotate(30deg);
+}
+
+.list {
+	@apply ml-12 mb-8 relative;
+}
+
+.list .check-icon {
+	@apply absolute w-8 text-4xl text-green-500;
+
+	transform: translateX(-135%);
+}
+
+.list .title {
+	@apply text-gray-700 font-light text-2xl;
+}
+
+.list .sub {
+	@apply mb-4 text-gray-600 text-sm;
+}
+
+@screen lg {
+	.helper {
+		@apply block;
+	}
+}
+</style>
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,12 +3,15 @@
 		<h1>Create</h1>
 		<div class="flex items-start mb-16">
 			<form @submit.prevent class="lg:mr-12 flex-grow">
-				<label class="label">Benefit</label>
+				<label class="label">Title</label>
+				<input v-model="ideaForm.title" class="input" type="text" />
+
+				<label class="label">Value</label>
 				<input
-					v-model="ideaForm.benefit"
+					v-model="ideaForm.value"
 					class="input"
 					type="text"
-					placeholder="What does your idea contribute?"
+					placeholder="What value does your idea bring?"
 				/>
 
 				<label class="label">Description</label>
@@ -19,8 +22,10 @@
 					placeholder="A detailed explanation of your idea to convice others that it's something to get excited about!"
 				/>
 
-				<label class="label">Difficulty</label>
-				<multiselect class="mb-8" v-model="ideaForm.difficulty" :options="['Easy', 'Medium', 'Hard']" />
+				<hr class="mb-8 border-b" />
+
+				<label class="label">Project Scale</label>
+				<multiselect class="mb-8" v-model="ideaForm.scale" :options="['Small', 'Medium', 'Large']" />
 
 				<div class="hours">
 					<div>
@@ -33,8 +38,6 @@
 					</div>
 				</div>
 
-				<hr class="mb-8 border-b" />
-
 				<label class="label">Skills required</label>
 				<multiselect
 					class="mb-8"
@@ -43,76 +46,12 @@
 					:multiple="true"
 					placeholder="Type to search"
 				>
-					<span slot="noResult">Oops! No skills found. Consider changing the search query.</span>
+					<span slot="noResult">Oops! No skills found. Try searching for something else.</span>
 				</multiselect>
 
 				<AppButton @click.native="add()">Add</AppButton>
 			</form>
-			<div class="helper">
-				<h2 class="mb-4">Make sure your idea is</h2>
-				<ul>
-					<li class="flex items-center">
-						<div class="rounded-full fill-current text-green-500">
-							<svg
-								class="w-8 h-8"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="3"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							>
-								<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-								<polyline points="22 4 12 14.01 9 11.01" />
-							</svg>
-						</div>
-						<span class="text-gray-700 font-light text-2xl ml-4">Descriptive</span>
-					</li>
-					<li
-						class="ml-12 text-gray-600 text-sm -mt-1 mb-8"
-					>Make sure you thoroughly outline why your idea is great and why others would want to contribute!</li>
-					<li class="flex items-center">
-						<div class="rounded-full fill-current text-green-500">
-							<svg
-								class="w-8 h-8"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="3"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							>
-								<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-								<polyline points="22 4 12 14.01 9 11.01" />
-							</svg>
-						</div>
-						<span class="text-gray-700 font-light text-2xl ml-4">Unique</span>
-					</li>
-					<li
-						class="ml-12 text-gray-600 text-sm -mt-1 mb-8"
-					>Check to see if someone has already put a similar idea up</li>
-					<li class="flex items-center">
-						<div class="rounded-full fill-current text-green-500">
-							<svg
-								class="w-8 h-8"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="3"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							>
-								<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-								<polyline points="22 4 12 14.01 9 11.01" />
-							</svg>
-						</div>
-						<span class="text-gray-700 font-light text-2xl ml-4">Achievable</span>
-					</li>
-					<li
-						class="ml-12 text-gray-600 text-sm -mt-1 mb-8"
-					>Check to see if someone has already put a similar idea up</li>
-				</ul>
-			</div>
+			<AppHelper />
 		</div>
 
 		<!-- <input v-model="ideaForm.skillsRequired" placeholder="required skills" type="text" /> -->
@@ -126,13 +65,15 @@ import { firestore } from "firebase/app";
 import * as AppTypes from "@/typings";
 
 import Multiselect from "vue-multiselect";
+import AppHelper from "@/components/Create/Helper.vue";
 
-@Component({ components: { Multiselect } })
+@Component({ components: { Multiselect, AppHelper } })
 export default class Create extends Vue {
 	// State
 	ideaForm: AppTypes.IdeaNew = {
-		difficulty: null,
-		benefit: null,
+		title: null,
+		scale: null,
+		value: null,
 		description: null,
 		skillsRequired: [],
 		timeEstimation: [null, null]
@@ -200,35 +141,6 @@ select.input {
 
 .hours > div:first-of-type {
 	@apply mr-8;
-}
-
-.helper {
-	@apply bg-gray-100 rounded-xl shadow-xl p-12 w-2/5 hidden overflow-hidden relative;
-}
-
-.helper > h2 {
-	@apply font-bold text-2xl;
-}
-
-.helper::before {
-	content: "";
-	@apply absolute block h-24 w-24 rounded-full bg-gray-400 opacity-75;
-	bottom: -1.75em;
-	left: 3em;
-}
-
-.helper::after {
-	content: "";
-	@apply absolute block h-8 w-4/5 bg-gray-400 opacity-75;
-	bottom: 2em;
-	left: -5em;
-	transform: rotate(30deg);
-}
-
-@screen lg {
-	.helper {
-		@apply block;
-	}
 }
 </style>
 
